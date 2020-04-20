cfb46edc2c2ad4137de9692e41683208b9888e2f
Chang Yun Son <scychon@gmail.com>
2020-04-21 02:28:46 +0900

    Add OKE Module to calculate dipole orientations

---
+++ oke/compile_omp.sh
@@ -0,0 +1,2 @@
+#ifort -qopenmp $GMXXTC/xtc-interface-wrap.o $OPENMMDCD/dcdmod.o $OPENMMDCD/trajmod.o variables.f90 topol.f90 fvector.f90 filehandle.f90 conductivity.f90 -o calc_cond_omp -lxdrfile -L $GMXXTC/lib -I $GMXXTC -I $OPENMMDCD -CB
+gfortran -fopenmp $GMXXTC/xtc-interface-wrap.o $OPENMMDCD/dcdmod.o $OPENMMDCD/trajmod.o variables.f90 topol.f90 fvector.f90 filehandle.f90 oke.f90 -o calc_oke_omp -lxdrfile -L $GMXXTC/lib -I $GMXXTC -I $OPENMMDCD -fcheck=bounds
